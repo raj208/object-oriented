92920fc7f90a63e8ff77c40745487674c19c374f
RAJENDRA <rajendrajfc1021@gmail.com>
2023-08-25 22:49:56 +0530

    changing directories

---
+++ basics/tut6.java
@@ -0,0 +1,10 @@
+public class tut6 {
+    static void myMethod(){
+        System.out.println("this is my new methos");
+    }
+    
+    public static void main (String[] args){
+        myMethod();
+    }
+
+}
